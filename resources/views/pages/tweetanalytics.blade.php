024bfba4c3dbd8a8ea8b1c90c753b892d2c3cf98
Sahan Munasinghe <munasingheTS93@gmail.com>
2016-09-16 23:33:58 +0530

    Finished tweet-analytics page basic structure -Sahan

--- resources/views/pages/tweetanalytics.blade.php
+++ resources/views/pages/tweetanalytics.blade.php
@@ -14,19 +14,82 @@
 
     <div class="container">
 
-        <div class="row">
+        <div class="row m-t-20 well">
 
-            {{--<div class="col-sm-12">--}}
-            {{--<div class="jumbotron">--}}
-            {{--<h1>Twitter analytics made easier!</h1>--}}
-            {{--<p>Offering you analytics to filter positive, and negative tweets</p>--}}
-            {{--</div>--}}
-            {{--</div>--}}
+            <div class="col-sm-1"></div>
 
-            <div class="jumbotron">
-                <input class="form-control" type="text">
+
+            <!-- --------------------------------------------------------------------------------------------------------------- -->
+            <div class="col-sm-10">
+                <div class="col-sm-12">
+                    <h4>Search Criteria:</h4>
+                </div>
+
+                <div class="col-sm-11 col-xs-10">
+                    <input id="search" class="form-control" type="text">
+                </div>
+
+                <div class="col-sm-1 col-xs-2">
+                    <a href="" class="btn btn-default">
+                        <i class="fa fa-search"></i>
+                    </a>
+                </div>
+            </div>
+            <!-- --------------------------------------------------------------------------------------------------------------- -->
+
+            <div class="col-sm-1"></div>
+
+
+        </div>
+
+        <div class="col-sm-12">
+            <hr>
+        </div>
+
+
+
+
+
+
+        <!-- ---------------- Pie Chart Section Start ------------------------------------------------------------------------------------------- -->
+        <div class="col-sm-12 well">
+            <h4>Pie Chart:</h4>
+        </div>
+        <!-- ---------------- Pie Chart Section End ------------------------------------------------------------------------------------------- -->
+
+        <div class="col-sm-12">
+            <hr>
+        </div>
+
+
+
+
+
+
+
+
+        <!-- -------------------- Good Bad Tweets Section Start --------------------------------------------------------------- -->
+        <div class="col-sm-12 well">
+            <h4>Top 5 Good & Bad:</h4>
+
+            <div class="col-sm-12">
+
+                <div class="col-sm-6">
+
+
+                </div>
+
+                <div class="col-sm-6">
+
+                </div>
             </div>
         </div>
+        <!-- ----------------------- Good Bad Tweets Section End ---------------------------------------------------------------------------------------- -->
+
+
+
+
+
     </div>
 
 
