b96c4427e723f753eaec013c4ce28eeffe0e97d4
munasinghets <Munasinghets93@gmail.com>
2016-10-28 19:31:46 +0530

    Added comments to LoginController.php - Sahan

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,27 +24,41 @@ class LoginController extends Controller {
      */
     public function checkCredentials(Request $request)
     {
+
+        /*
+         * Get the data coming form the request
+         * and assign them to local variables
+         */
         $email = $request->input('email');
         $password = $request->input('password');
 
+        // The codes used to send the response status
         $responseCode = new TweetUResponseCode();
 
+        // Declare a user
         $user = new User;
 
 
         try{
 
+            // Instantiate the user if it exists in the database
             $user = User::where('email', $email)
                 ->get()->first();
 
+            // Check if the user exists
             if($user !== null)
             {
+                /*
+                 * The secondary search only happens if the user exists.
+                 * Went for this approach hoping to minimize needless searching
+                 */
                 $user = User::where('email', $email)
                     ->where('password', $password)
                     ->get()->first();
 
 
 
+                // Check if the secondary search gives any results
                 if($user !== null)
                 {
                     $response = array(
@@ -56,6 +70,7 @@ class LoginController extends Controller {
                     return json_encode($response);
                 }
 
+                // Secondary search returning no results only means that the password was incorrect
                 else{
                     $response = array(
                         'status' => $responseCode->error,
@@ -66,6 +81,7 @@ class LoginController extends Controller {
                 }
             }
 
+            // Primary search failure means that the user does not exist in the database
             else
             {
                 $response = array(
@@ -89,7 +105,7 @@ class LoginController extends Controller {
             return json_encode($response);
         }
 
-        
+
     }
 
 }
