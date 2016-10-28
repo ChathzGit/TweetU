c4169bb9e71e86ab6e5815f26d44832d07c230fb
munasinghets <Munasinghets93@gmail.com>
2016-10-28 19:47:53 +0530

    Added extra error handling and comments to UserAccountController.php - Sahan

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\Util\TweetUResponseCode;
 
 use App\User;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Unirest\Exception;
 
@@ -33,16 +34,25 @@ class UserAccountController extends Controller
      */
     public function saveUser(Request $request)
     {
+
+        // Create empty user
         $user = new User;
 
+        /*
+         * Get the data from the request and assign the
+         * values into local variables
+         */
         $user->name = $request->input('name');
         $user->email = $request->input('email');
         $user->password = $request->input('password');
 
 
+        // Response code that will be sent to the front end
         $responseCode = new TweetUResponseCode();
 
         try {
+
+            // Save the user to the database, or return error
             if ($user->save()) {
                 $response = array(
                     'status' => $responseCode->success
@@ -53,18 +63,42 @@ class UserAccountController extends Controller
                 );
             }
 
-
             return json_encode($response);
 
-        } catch (Exception $e) {
+        } catch (QueryException $e) {
+
+            // Check if the QueryException is an integrity violation
+           if($e->getCode() === '23000')
+           {
+               $response = array(
+                   'status' => $responseCode->error,
+                   'error' => "User with the same email already exists"
+               );
+
+               return json_encode($response);
+           }
+
+           // Check if the QueryException is any other violation
+            else{
+
+                $response = array(
+                    'status' => $responseCode->error,
+                    'error' => "Error creating account"
+                );
+
+                return json_encode($response);
+            }
+        }
+
+        // Catch all other exceptions
+        catch (Exception $e) {
 
             $response = array(
                 'status' => $responseCode->error,
-                'error' => $e
+                'error' => "Error creating account"
             );
 
             return json_encode($response);
-
         }
     }
 
