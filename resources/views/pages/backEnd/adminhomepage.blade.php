01ae04f986b9d67d3f360e849ce62487d3765140
munasinghets <Munasinghets93@gmail.com>
2016-11-23 19:06:23 +0530

    admin home page UI refinements done - Sahan

--- resources/views/pages/backEnd/adminhomepage.blade.php
+++ resources/views/pages/backEnd/adminhomepage.blade.php
@@ -25,12 +25,15 @@
 
                 <div class="col-sm-12 m-b-20">
                     <h3><strong>Total Usage Statistics:</strong></h3>
+
+                    <p class="text-grey hidden-xs">The total number of times each service was used since the site launch</p>
+                    <p class="text-center visible-xs text-grey">The total number of times each service was used since the site launch</p>
                 </div>
 
 
                 <div class="col-sm-3 col-xs-6 m-b-5 text-center">
 
-                    <div class="col-sm-12 selected-profile p-0">
+                    <div class="col-sm-12 admin-hp-usagecontainer selected-profile p-0">
 
                         <div class="col-sm-12 p-l-30 p-r-30">
                             <img class="img img-responsive img-hp-thumb"
@@ -49,7 +52,7 @@
 
                 <div class="col-sm-3 col-xs-6 m-b-5 text-center">
 
-                    <div class="col-sm-12 selected-profile p-0">
+                    <div class="col-sm-12 admin-hp-usagecontainer selected-profile p-0">
 
                         <div class="col-sm-12 p-l-30 p-r-30">
                             <img class="img img-responsive img-hp-thumb"
@@ -69,7 +72,7 @@
 
                 <div class="col-sm-3 col-xs-6 m-b-5 text-center">
 
-                    <div class="col-sm-12 selected-profile p-0">
+                    <div class="col-sm-12 admin-hp-usagecontainer selected-profile p-0">
 
                         <div class="col-sm-12 p-l-30 p-r-30">
                             <img class="img img-responsive img-hp-thumb"
@@ -89,7 +92,7 @@
 
                 <div class="col-sm-3 col-xs-6 m-b-5 text-center">
 
-                    <div class="col-sm-12 selected-profile p-0">
+                    <div class="col-sm-12 admin-hp-usagecontainer selected-profile p-0">
 
                         <div class="col-sm-12 p-l-30 p-r-30">
                             <img class="img img-responsive img-hp-thumb"
@@ -115,107 +118,13 @@
         </div>
 
 
-        <div class="row" hidden>
-            <div class="col-lg-3 col-md-6">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-comments fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9 text-right">
-                                <div class="huge"><% AllTweets %></div>
-                                <div>New Visits!</div>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="#">
-                        <div class="panel-footer">
-                            <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-
-                            <div class="clearfix"></div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="panel panel-green">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-tasks fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9 text-right">
-                                <div class="huge">12</div>
-                                <div>New Searches!</div>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="#">
-                        <div class="panel-footer">
-                            <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-
-                            <div class="clearfix"></div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="panel panel-yellow">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-shopping-cart fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9 text-right">
-                                <div class="huge">124</div>
-                                <div>New Comparisons!</div>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="#">
-                        <div class="panel-footer">
-                            <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-
-                            <div class="clearfix"></div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="panel panel-red">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-support fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9 text-right">
-                                <div class="huge">13</div>
-                                <div>New Accounts!</div>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="#">
-                        <div class="panel-footer">
-                            <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-
-                            <div class="clearfix"></div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <!-- /.row -->
-
-
         <div class="row">
             <div class="col-sm-12 col-xs-12">
                 <div class="col-sm-12">
                     <h3><strong>Site Usage This Month:</strong></h3>
+
+                    <p class="text-grey hidden-xs">The number of times each service was used in the last 30 days.</p>
+                    <p class="text-center visible-xs text-grey">The number of times each service was used in the last 30 days.</p>
                 </div>
 
                 <div class="col-sm-12">
@@ -233,61 +142,65 @@
             <div class="col-sm-12 col-xs-12">
                 <div class="col-sm-12 m-b-20">
                     <h3><strong>Percentage Usage This Month:</strong></h3>
+
+                    <p class="text-grey hidden-xs">A percentage comparison of the usage of services between the last 30 days and the 30 days before that.</p>
+                    <p class="text-center visible-xs text-grey">A percentage comparison of the usage of services between the last 30 days and the 30 days before that.</p>
                 </div>
                 <!-- /.panel-heading -->
 
                 <div class="col-sm-12">
 
                     <div class="col-sm-3 m-b-5">
-
-                        <div class="col-sm-12 selected-profile">
+                        <div class="col-sm-12 admin-hp-percentagecontainer text-center selected-profile">
                             <h4><strong>Tweet Analysis</strong></h4>
-                                <span ng-class="{'c-green': TweetPercentage >= 100, 'c-red': TweetPercentage < 100 }"><% TweetPercentage %>
-                                    %</span>
+                                <span ng-class="{'c-green': TweetPercentage >= 100, 'c-red': TweetPercentage < 100 }">
+                                    <strong><% TweetPercentage %>%</strong>
+                                </span>
                             <hr>
-                        <span ng-show="TweetPercentage >= 100">A <% TweetPercentage %>
+                        <span class="text-grey" ng-show="TweetPercentage >= 100">A <% TweetPercentage %>
                             % increase in Tweet analytic usage</span>
-                            <span ng-show="TweetPercentage == 0">Percentage not available</span>
-                            <span ng-show="TweetPercentage < 100 && TweetPercentage != 0">A <% TweetPercentage %>% decrease in tweet analytic usage</span>
+                            <span class="text-grey" ng-show="TweetPercentage == 0">Percentage not available</span>
+                            <span class="text-grey" ng-show="TweetPercentage < 100 && TweetPercentage != 0">A <% TweetPercentage %>% decrease in tweet analytic usage</span>
                         </div>
                     </div>
 
                     <div class="col-sm-3 m-b-5">
-                        <div class="col-sm-12 selected-profile">
-                            <h4>Account Analysis</h4>
-                                <span ng-class="{'c-green': AccountPercentage >= 100, 'c-red': AccountPercentage < 100 }"><% AccountPercentage %>
-                                    %</span>
+                        <div class="col-sm-12 admin-hp-percentagecontainer text-center selected-profile">
+                            <h4><strong>Account Analysis</strong></h4>
+                                <span ng-class="{'c-green': AccountPercentage >= 100, 'c-red': AccountPercentage < 100 }">
+                                    <strong><% AccountPercentage %>%</strong>
+                                </span>
                             <hr>
-                <span ng-show="AccountPercentage >= 100">A <% AccountPercentage %>
+                <span class="text-grey" ng-show="AccountPercentage >= 100">A <% AccountPercentage %>
                     % increase in Tweet analytic usage</span>
-                            <span ng-show="AccountPercentage == 0">Percentage not available</span>
-                                <span ng-show="AccountPercentage < 100 && AccountPercentage != 0">A <% AccountPercentage %>
+                            <span class="text-grey" ng-show="AccountPercentage == 0">Percentage not available</span>
+                                <span class="text-grey" ng-show="AccountPercentage < 100 && AccountPercentage != 0">A <% AccountPercentage %>
                                     % decrease in account analytic usage</span>
                         </div>
                     </div>
 
                     <div class="col-sm-3 m-b-5">
-                        <div class="col-sm-12 selected-profile">
-                            <h4>Topic Comparisons</h4>
+                        <div class="col-sm-12 admin-hp-percentagecontainer text-center selected-profile">
+                            <h4><strong>Topic Comparisons</strong></h4>
                                 <span ng-class="{'c-green': TopicComparisonPercentage >= 100, 'c-red': TopicComparisonPercentage < 100 }"><% TopicComparisonPercentage %>
                                     %</span>
                             <hr>
-                            <span ng-show="TopicComparisonPercentage >= 100">A <% TopicComparisonPercentage %>% increase in Tweet analytic usage</span>
-                            <span ng-show="TopicComparisonPercentage == 0">Percentage not available</span>
-                                <span ng-show="TopicComparisonPercentage < 100 && TopicComparisonPercentage != 0">A <% TopicComparisonPercentage %>
+                            <span class="text-grey" ng-show="TopicComparisonPercentage >= 100">A <% TopicComparisonPercentage %>% increase in Tweet analytic usage</span>
+                            <span class="text-grey" ng-show="TopicComparisonPercentage == 0">Percentage not available</span>
+                                <span class="text-grey" ng-show="TopicComparisonPercentage < 100 && TopicComparisonPercentage != 0">A <% TopicComparisonPercentage %>
                                     % decrease in comparisons done</span>
                         </div>
                     </div>
 
                     <div class="col-sm-3 m-b-5">
-                        <div class="col-sm-12 selected-profile">
-                            <h4>Account Comparisons</h4>
+                        <div class="col-sm-12 admin-hp-percentagecontainer text-center selected-profile">
+                            <h4><strong>Account Comparisons</strong></h4>
                                 <span ng-class="{'c-green': AccountComparisonPercentage >= 100, 'c-red': AccountComparisonPercentage < 100 }"><% AccountComparisonPercentage %>
                                     %</span>
                             <hr>
-                            <span ng-show="AccountComparisonPercentage >= 100">A <% AccountComparisonPercentage %>% increase in Tweet analytic usage</span>
-                            <span ng-show="AccountComparisonPercentage == 0">Percentage not available</span>
-                                <span ng-show="AccountComparisonPercentage < 100 && AccountComparisonPercentage != 0">A <% AccountComparisonPercentage %>
+                            <span class="text-grey" ng-show="AccountComparisonPercentage >= 100">A <% AccountComparisonPercentage %>% increase in Tweet analytic usage</span>
+                            <span class="text-grey" ng-show="AccountComparisonPercentage == 0">Percentage not available</span>
+                                <span class="text-grey" ng-show="AccountComparisonPercentage < 100 && AccountComparisonPercentage != 0">A <% AccountComparisonPercentage %>
                                     % decrease in comparisons done</span>
                         </div>
                     </div>
